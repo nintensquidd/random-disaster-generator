000d321939550f3b06f4c3408a70ae33657c2968
Glitch (thimble-import) <none>
2019-01-02 21:12:28 +0000

    🐴😼 Checkpoint ./README.md:320013/416

--- README.md
+++ README.md
@@ -1,11 +1,1 @@
-Glitch Thimble Importer
-=======================
-
-Please wait while we import your app...
-
-More Info
----------
-
-Remix this app with your Thimble project id and token and it will import that app and run it!
-
-    https://glitch.com/edit/#!/remix/thimble-import?TOKEN=$token&ID=$projectId
+# Homework Excuse Generator
---
+++ excuses.js
@@ -0,0 +1,18 @@
+function Excuse() {
+
+  var myDog = ['dog', 'cat', 'sense of ennui', 'hamster', 'chinchilla', 'iguana', 'turtle', 'best friend', 'bro', 'boo', 'crush', 'sister', 'brother', 'nemesis', 'doppelganger', 'gerbil', 'bunny', 'fish', 'crew'];
+  
+  var ate = ['ate', 'peer-reviewed', 'destroyed', 'deleted', 'erased', 'remixed', 'twitched', 'recycled', 'livetweeted', 'undermined', 'underbid', 'upcycled', 'gave away', 'plagiarised', 'confiscated', 'barfed on', 'stole', 'ransomed', 'spilled water on', 'lost', 'misplaced', 'buried', 'shredded', 'hid', 'liberated', 'left with', 'ebayed', 'craigslisted', 'etsyed', 'tweeted', 'instagrammed', 'snapchatted', 'youtubed', 'dunked on', 'subteweeted', 'favorited', 'upvoted', 'downvoted', 'live cast', 'pinned', 'vlogged', 'blogged about'];
+  
+  var myHomework = ['homework', 'project', 'essay', 'film project', 'visualization', 'infographic', 'pre-writing', 'term paper', 'lab report', 'reading log', 'summary', 'article', 'flash cards', 'study guide', 'textbook', 'workbook', 'reading book', 'diorama', 'poster', 'dreams', 'laptop', 'computer', 'tablet', 'momentum', 'report', 'slideshow', 'slide deck', 'presentation', 'worksheet', 'handout', 'notes', 'portfolio', 'blog post', 'comment'];
+
+  function pickRandom(wordArray) {
+    var lengthOfArray = wordArray.length - 1;
+    var randomNumber = Math.round(Math.random() * lengthOfArray);
+    return wordArray[randomNumber];
+  }
+
+  var newExcuse = 'My ' + pickRandom(myDog) + ' ' + pickRandom(ate) + ' my ' + pickRandom(myHomework) + '.';
+  document.getElementById('excuses').innerHTML = newExcuse;
+
+}
