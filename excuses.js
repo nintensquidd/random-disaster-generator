4c186e6cc992e51e63eebdb2aa6c7f37d414a1de
Glitch (thimble-import) <none>
2020-08-07 02:16:22 +0000

    🐵🐢 Checkpoint ./excuses.js:20688559/858 ./index.html:20688559/55 ./tutorial.html:20688559/172 ./.glitch-assets:20688559/425 ./style.css:20688559/123

--- excuses.js
+++ excuses.js
@@ -1,10 +1,10 @@
 function Excuse() {
 
-  var myDog = ['puppy', 'kitten', 'penguin', 'hamster', 'transformer', 'iguana', 'turtle', 'tornado', 'bro', 'ghost', 'friendly neighbor', 'homer simpson', 'asteroid', 'mp4 file', 'log of feces', 'finger', 'arrow', 'cursor', 'crew sock'];
+  var myDog = ['puppy', 'kitten', 'penguin', 'hamster', 'transformer', 'iguana', 'turtle', 'tornado', 'Tyler Blevins' , 'ghost', 'friendly neighbor', 'homer simpson', 'asteroid', 'mp4 file', 'log of feces', 'finger', 'arrow', 'cursor', 'crew sock'];
   
-  var ate = ['demolished', 'crumbled', 'destroyed', 'deleted', 'erased', 'made art out of', 'absolutely wrecked', 'went to town on', 'didnt like', 'beat up', 'terrorized', 'accidentally knocked over', 'borrowed', 'got envious over', 'confiscated', 'barfed on', 'stole', 'ransomed', 'spilled water on', 'lost', 'misplaced', 'buried', 'shredded', 'hid', 'liberated', 'left with', 'ebayed', 'craigslisted', 'etsyed', 'dunked on', 'subteweeted', 'downvoted', '', 'pinned', 'vlogged', 'blogged about'];
+  var ate = ['demolished', 'crumbled', 'destroyed', 'deleted', 'erased', 'made art out of', 'absolutely wrecked', 'went to town on', 'didnt like', 'beat up', 'terrorized', 'accidentally knocked over', 'borrowed', 'got envious over', 'confiscated', 'barfed on', 'stole', 'ransomed', 'spilled water on', 'lost', 'misplaced', 'buried', 'shredded', 'hid', 'liberated', 'left with', 'ebayed', 'craigslisted', 'etsyed', 'dunked on', 'subteweeted', 'downvoted', 'blocked', 'goatsed'];
   
-  var myHomework = ['homework', 'project', 'essay', 'film project', 'visualization', 'infographic', 'pre-writing', 'term paper', 'lab report', 'reading log', 'summary', 'article', 'flash cards', 'study guide', 'textbook', 'workbook', 'reading book', 'diorama', 'poster', 'dreams', 'laptop', 'computer', 'tablet', 'momentum', 'report', 'slideshow', 'slide deck', 'presentation', 'worksheet', 'handout', 'notes', 'portfolio', 'blog post', 'comment'];
+  var myHomework = ['empire state building', 'city of tokyo', 'eiffel tower', 'sears tower', 'chicago bean', 'st louis arch', 'entire state of wyoming', 'twighlight zone', 'twitter userbase', 'house at the end of the street', 'whole internet', 'important data place', 'dojima family building', 'eva unit 01', 'borders book store', 'sports field', 'fortnite server room', 'subreddit', 'post you just made', 'literal pizza hut', 'chromebook holder'];
 
   function pickRandom(wordArray) {
     var lengthOfArray = wordArray.length - 1;
@@ -12,7 +12,7 @@ function Excuse() {
     return wordArray[randomNumber];
   }
 
-  var newExcuse = 'a giant'+ pickRandom(myDog) + pickRandom(ate) + 'the' + pickRandom(myHomework) + '!!!';
+  var newExcuse = 'a giant' + pickRandom(myDog) + pickRandom(ate) + 'the' + pickRandom(myHomework) + '!!!';
   document.getElementById('excuses').innerHTML = newExcuse;
 
 }
